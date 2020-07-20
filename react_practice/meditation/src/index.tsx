79f9d2e7c046a2dd8232d5e05ddecb12db789cb0
JSKeum <JSKeum2512@gmail.com>
2020-07-20 18:25:18 +0900

    add Suspense to Router

--- react_practice/meditation/src/index.tsx
+++ react_practice/meditation/src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path={["/main", "/"]} component={App} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path={["/main", "/"]} component={App} />
+      </Switch>
+    </Suspense>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
